f0b2daad24a62f13de31a31c9d5b2bb280b12a73
csekri <csehkristof@gmail.com>
2022-01-29 13:51:40 +0000

    Readme fixes, test_fold renamed to example

--- draw.py
+++ draw.py
@@ -9,12 +9,12 @@ lgh.draw_hist(inverse=False)
 # The resolution of the output image can be increased with the parameter below
 resolution = 500
 map = mapnik.Map(resolution, resolution)
-mapnik.load_map(map, 'uni_colour.xml')
+mapnik.load_map(map, 'buildings.xml')
 area = mapnik.Box2d(*lgh.area)
 p = mapnik.Projection("+init=epsg:3857")
 map.aspect_fix_mode = mapnik.aspect_fix_mode.ADJUST_CANVAS_HEIGHT
 map.zoom_to_box(p.forward(area))
-#map.zoom_all()
-#bbox = p.forward(map.envelope())
+# map.zoom_all()
+# bbox = p.forward(map.envelope())
 mapnik.render_to_file(map, str(lgh) + '.png', 'png')
 print ('rendered image to "world.png"')
